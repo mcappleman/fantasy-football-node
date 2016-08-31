e4791d24739b5b8b08ba56079e54ef4db6414753
Matt Cappleman <Matt Cappleman>
2016-08-30 23:07:07 -0400

    Updated the js to print off more info

--- GatherPreviousData.js
+++ GatherPreviousData.js
@@ -15,20 +15,24 @@ function readFile(path, year) {
 	rd.on('close', function() {
 		filesDone[year] = true;
 		if(filesDone[currentYear-1] && filesDone[currentYear-2] && filesDone[currentYear-3]){
+			for(var player in playerData) {
+				playerData[player] = getAverages(playerData[player]);
+			}
+			rankPlayers('VBDAverage', 'PointsAverage');
 			writeFiles();
 		}
 	});
 }
 
 function writeFiles() {
-	fs.writeFileSync('AllPlayers.csv', 'Name,Position,PointsAverage,VBDAverage,BestPointsYear,BestVBDYear \n', 'utf8', ErrorCallback);
-	for(var player in playerData){
-		if(playerData[player].name )
-		playerData[player] = getAverages(playerData[player]);
+	var header_line = 'Taken,Rank,Name,Position,PointsAverage,VBDAverage,BestPointsYear,BestVBDYear,' + 
+					  'Points_2015,VBD_2015,Team_2015,Points_2014,VBD_2014,Team_2014,Points_2013,VBD_2013,Team_2013 \n';
+	fs.writeFileSync('AllPlayers.csv', header_line, 'utf8', ErrorCallback);
+	for(var player in playerData) {
 		var writableLine = getCSVLine(playerData[player]) + '\n';
 		fs.appendFileSync('AllPlayers.csv', writableLine, 'utf8');
 	}
-	console.log(playerData['BrowAn04']);
+	console.log(playerData['BrowAn04'], playerData['FreeDe00']);
 }
 
 function ErrorCallback(err) {
@@ -36,14 +40,32 @@ function ErrorCallback(err) {
 }
 
 function getCSVLine(player) {
-	var str = player.name + ',' + player.position + ',' + player.fantasyAverage + ',' + player.VBDAverage + ',';
+	var str = ',' + player.rank + ',' + player.name + ',' + player.position + ',' + player.fantasyAverage + ',' + player.VBDAverage + ',';
 	if(player.bestFantasyYear >= furthestYear) {
 		str += player.bestFantasyYear + ' ' + player[player.bestFantasyYear]['team'] + ',';
 	} else {
 		str += ',';
 	}
 	if(player.bestVBDYear >= furthestYear) {
-		str += player.bestVBDYear + ' ' + player[player.bestVBDYear]['team'];
+		str += player.bestVBDYear + ' ' + player[player.bestVBDYear]['team'] + ',';
+	} else {
+		str += ',';
+	}
+	str += getYearStats(currentYear-1, player);
+	str += getYearStats(currentYear-2, player);
+	str += getYearStats(currentYear-3, player);
+	return str;
+}
+
+function getYearStats(year, player){
+	var str = '';
+	if(player[year]){
+		str += player[year]['points'] + ',' + player[year]['VBD'] + ',' + player[year]['team'];
+	} else {
+		str += ',,';
+	}
+	if(year !== currentYear-3){
+		str += ',';
 	}
 	return str;
 }
@@ -53,7 +75,7 @@ function addLine(line, year) {
 		var lineObject = getLineObject(line);
 		var playerId = lineObject.id;
 		if(playerData[playerId] === undefined){
-			playerData[playerId] = {name: lineObject.name, position: lineObject.position};
+			playerData[playerId] = {id: playerId, name: lineObject.name, position: lineObject.position};
 		}
 		playerData[playerId][year] = {points: lineObject.points, VBD: lineObject.VBD, team: lineObject.team}
 	}
@@ -121,3 +143,38 @@ var filesDone = {};
 readFile('./PlayerStats' + (currentYear - 1) + '.csv', currentYear - 1);
 readFile('./PlayerStats' + (currentYear - 2) + '.csv', currentYear - 2);
 readFile('./PlayerStats' + (currentYear - 3) + '.csv', currentYear - 3);
+
+
+function rankPlayers(rankKey, secondaryRankKey) {
+	var rankedList = [];
+	for(var player in playerData) {
+		rankedList.push(playerData[player]);
+	}
+
+
+	for(var i = 0; i < rankedList.length; i++) {
+		for(var j = i+1; j < rankedList.length; j++) {
+			var currentPlayer = rankedList[i];
+			var comparedPlayer = rankedList[j];
+			if(Number(rankedList[j][rankKey]) > Number(rankedList[i][rankKey])) {
+				if(rankedList[i].id === 'GronRo00'){
+					console.log(Number(rankedList[j][rankKey]));
+				}
+				var temp = rankedList[i];
+				rankedList[i] = rankedList[j];
+				rankedList[j] = temp;
+			} else if(Number(rankedList[i][rankKey]) === Number(rankedList[j][rankKey])) {
+				if(Number(rankedList[j][secondaryRankKey]) > Number(rankedList[i][secondaryRankKey])) {
+					var temp = rankedList[i];
+					rankedList[i] = rankedList[j];
+					rankedList[j] = temp;
+				}
+			} 
+		}
+	}
+
+	rankedList.forEach(function(player, index){
+		playerData[player.id].rank = index+1;
+	});
+
+}
